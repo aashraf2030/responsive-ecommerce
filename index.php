fbc8ca5764d297a27359b5cb58a2e4a2e4c67633
Ahmed Ashraf <81390978+aashraf2030@users.noreply.github.com>
2025-02-08 13:58:14 +0200

    Update index.php

--- index.php
+++ index.php
@@ -10,9 +10,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200..1000&display=swap" rel="stylesheet">
-    <style>
-       
-    </style>
+    <link rel="stylesheet" href="style.css /">
 </head>
 <body class="bg-white text-gray-900">
     <div id="overlay"></div>
